c7a78d3a84eca99852a86a7081d85cf2b9fad553
gracychadha <chadhagracy8@gmail.com>
2026-05-08 10:43:26 +0530

    test implement

--- src/components/test/TestComponent.jsx
+++ src/components/test/TestComponent.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function TestComponent() {
-  const STUDENT_URL = import.meta.env.VITE_BASE_URL;
+  const STUDENT_URL = import.meta.env.VITE_BASE_URL.replace("api/public","student");
   const navigate = useNavigate();
 
   const [isOtpScreen, setIsOtpScreen] = useState(false);
